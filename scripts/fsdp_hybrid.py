5be1ecd2975e3521f4c410c341df8a9dec30fe23
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2024-04-30 13:02:38 +0300

    Lots of updates, probably broke things - training config with pydantic - llama-like model args - beginning actual dataloader work

--- scripts/fsdp_hybrid.py
+++ scripts/fsdp_hybrid.py
@@ -2,6 +2,9 @@ import os
 import gc
 import time
 
+from dataclasses import dataclass
+from typing import Any, Dict
+
 import torch
 import torch.distributed as dist
 from schedulefree import AdamWScheduleFree
@@ -11,9 +14,12 @@ from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp import MixedPrecision, ShardingStrategy
 from torch.distributed.fsdp.wrap import ModuleWrapPolicy
 from torch.distributed.elastic.multiprocessing.errors import record
+from torch.distributed.checkpoint.stateful import Stateful
 from torch.profiler import ProfilerActivity, profile, schedule
 from torch.utils.flop_counter import FlopCounterMode
 import torch.nn.functional as F
+
+from pydantic import BaseModel, Field, ConfigDict
 
 from maester.log_utils import logger, init_logger
 from maester.memory import (cleanup_before_training,
@@ -23,10 +29,52 @@ from maester.model import (Attention, FeedForward, ModelArgs, Transformer,
 from maester.utils import transformer_flops
 from maester.datasets import build_hf_data_loader, create_tokenizer
 
-max_grad_norm = 1.0
-gc_freq = 1
+class Config(BaseModel):
+    model_config = ConfigDict(frozen=True)
 
-# TODO: does this do much/anything?
+    max_grad_norm = 1.0
+    gc_freq = 1
+    shard_size = 8
+    train_batch_size = 1
+    train_num_batches = 20
+    compile = True
+
+    # model
+    model_name = "test" # name of model args in model.py
+    block_size = 8192
+
+    # optimizer
+    opt_class = AdamWScheduleFree
+    opt_cfg = dict( # TODO: don't use dict, not validateable
+        lr = 1e-5,
+        betas = (0.9, 0.999)
+    )
+
+    # fsdp
+    sharding_strategy = ShardingStrategy.HYBRID_SHARD
+    auto_wrap_policy = ModuleWrapPolicy({Transformer, TransformerBlock})
+    backward_prefetch = BackwardPrefetch.BACKWARD_PRE
+    mixed_precision = MixedPrecision(param_dtype=torch.bfloat16, reduce_dtype=torch.float32, buffer_dtype=torch.bfloat16)
+
+    # activation checkpointing
+    activation_checkpointing = True
+    ac_auto_wrap_policy = {TransformerBlock} # TODO: naming? selective ac?
+
+
+@dataclass
+class TrainState(Stateful):
+    step: int = 0
+
+    def state_dict(self) -> dict[str, Any]:
+        return {
+            "step": torch.tensor(self.step, dtype=torch.int32),
+        }
+    
+    def load_state_dict(self, state_dict: dict[str, Any]) -> None:
+        self.step = state_dict["step"].item()
+
+
+# TODO: do these do much/anything?
 torch._inductor.config.coordinate_descent_tuning = True
 torch._inductor.config.triton.unique_kernel_names = True
 torch._inductor.config.fx_graph_cache = True # Experimental feature to reduce compilation times, will be on by default in future
@@ -41,15 +89,15 @@ def main():
     init_logger()
     logger.info(f"Starting training.")
 
+    cfg = Config() # TODO: enable configuring?
+
     # take control of garbage collection to avoid stragglers
-    _gc_freq = gc_freq
     gc.disable()
     gc.collect(1)
 
     # create a sharding plan for hybrid fsdp
-    shard_size = 8 # move to config
-    replicate_size = _world_size // shard_size
-    device_mesh = init_device_mesh("cuda", (replicate_size, shard_size), mesh_dim_names=("replicate", "shard"))
+    replicate_size = _world_size // cfg.shard_size
+    device_mesh = init_device_mesh("cuda", (replicate_size, cfg.shard_size), mesh_dim_names=("replicate", "shard"))
 
     logger.info(f"Device Mesh created: {device_mesh=}")
     dp_mesh = device_mesh["replicate"]
@@ -57,60 +105,57 @@ def main():
 
     # create model and move it to GPU with id rank
     with torch.device("meta"):
-        model_args = ModelArgs.from_name("Mistral-7B") # move model config to config
-        model_args.block_size = 4096 # move to config
+        model_args = ModelArgs.from_name(cfg.model_name)
+        model_args.block_size = cfg.block_size
         base_model = Transformer(model_args)
         n_params = sum([p.numel() for p in base_model.parameters()]) # before sharding, otherwise we only get per-rank params
         logger.info(f"Num params: {n_params}")
 
     # Init FSDP, not on meta
-    sharded_model = FSDP(base_model, device_mesh=device_mesh, use_orig_params=True, # move *some* fsdp things to config
-                        auto_wrap_policy=ModuleWrapPolicy({Transformer, TransformerBlock}),
-                        sharding_strategy=ShardingStrategy.HYBRID_SHARD,
-                        backward_prefetch=BackwardPrefetch.BACKWARD_PRE, # PRE is faster, uses more mem
-                        mixed_precision=MixedPrecision(param_dtype=torch.bfloat16, reduce_dtype=torch.float32, buffer_dtype=torch.bfloat16))
+    sharded_model = FSDP(base_model, device_mesh=device_mesh, use_orig_params=True,
+                        auto_wrap_policy=cfg.auto_wrap_policy,
+                        sharding_strategy=cfg.sharding_strategy,
+                        backward_prefetch=cfg.backward_prefetch,
+                        mixed_precision=cfg.mixed_precision)
 
     with torch.device("cuda"):
         sharded_model.setup_caches(32, model_args.block_size) # setup rope cache on gpu
     for param in sharded_model.parameters():
         assert param.device.type == "cuda"
 
-    if False: # activation checkpointing, move to config
+    if cfg.activation_checkpointing:
         # set_activation_checkpointing(sharded_model, auto_wrap_policy={Attention, FeedForward})
-        set_activation_checkpointing(sharded_model, auto_wrap_policy={TransformerBlock})
+        set_activation_checkpointing(sharded_model, auto_wrap_policy=cfg.ac_auto_wrap_policy)
         logger.info("Activation checkpointing enabled")
 
     logger.info(f"Model after parallelization {sharded_model=}\n")
-
-    model_name = ""
 
     # build tokenizer
     tokenizer_type = "tiktoken"
     tokenizer = create_tokenizer(tokenizer_type, "src/maester/datasets/tokenizer/original/tokenizer.model")
 
     # build dataloader
-    num_iterations = 20 # move to config
-    batch_size = 1
     dp_degree = dp_mesh.size()
     dp_rank = dp_mesh.get_local_rank()
     data_loader = build_hf_data_loader(
         "c4_mini",
         "src/maester/datasets/c4_mini",
         tokenizer,
-        batch_size,
+        cfg.train_batch_size,
         model_args.block_size,
         dp_degree,
         dp_rank,
     )
 
     lr = 3e-4
-    optimizer = AdamWScheduleFree(sharded_model.parameters(), lr=lr) # torch.optim.AdamW(sharded_model.parameters(), lr=lr, foreach=False, fused=True)
+    optimizer = cfg.opt_class(sharded_model.parameters(), **cfg.opt_cfg) # torch.optim.AdamW(sharded_model.parameters(), lr=lr, foreach=False, fused=True)
 
     # Training loop
     logger.info("\nStarting 2D training...")
     
     # compile the sharded model
-    # sharded_model = torch.compile(sharded_model, backend="inductor", mode="default")
+    if cfg.compile:
+        sharded_model = torch.compile(sharded_model, backend="inductor", mode="default")
 
     # get HFU and MFU flops
     # flop_counter = FlopCounterMode(display=False)
@@ -123,7 +168,7 @@ def main():
     L, H, Q, T = model_args.n_layer, model_args.n_head, model_args.head_dim, model_args.block_size
     mfu_flops_per_bs = 6*n_params*T + 3*(4*L*H*Q*(T**2))/2
     logger.info(f"MFU FLOPS: {mfu_flops_per_bs / 1e12}T")
-    new_mfu_flops = transformer_flops(model_args, batch_size, T, is_causal=True) / batch_size
+    new_mfu_flops = transformer_flops(model_args, cfg.train_batch_size, T, is_causal=True) / cfg.train_batch_size
     logger.info(f"New MFU FLOPS: {new_mfu_flops / 1e12}T")
 
     # loss fn can be shared by pipeline-parallel or non-pp execution
@@ -146,27 +191,31 @@ def main():
             p.export_memory_timeline("profiler/mem_.html")
             p.export_stacks("profiler/stacks.stacks", metric="self_cuda_time_total")
 
+    train_state = TrainState()
+
+    # training loop
     cleanup_before_training()
     sharded_model.train()
     optimizer.train()
 
+    # TODO: checkpointing
+    # checkpoint = CheckpointManager(
+    #     model=model,
+    #     optimizer=optimizer,
+    #     lr_scheduler=scheduler,
+    #     states={"train_state": train_state},
+    #     job_config=job_config,
+    # )
+    # checkpoint.load()
+
     data_iterator = iter(data_loader)
 
-    with profile(
-        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-        schedule=profiler_schedule,
-        profile_memory=True,
-        with_flops=True,
-        with_stack=True,
-        record_shapes=True,
-        on_trace_ready=trace_handler
-    ) as prof:
-        for i in range(num_iterations):
-            logger.info("Start of iteration {i}.")
+    logger.info(f"Training starts at step {train_state.step + 1}")
+    with maybe_enable_profiling(cfg) as torch_profiler:
+        # checkpoint.reset() # TODO
+        while train_state.step < cfg.train_num_batches:
             # seeding with dp_rank to ensure identical inputs for TP groups
             torch.manual_seed(i + dp_rank)
-            #inp = torch.randint(size=(batch_size, model_args.block_size), low=0, high=128, device="cuda", dtype=torch.int)
-            #labels = torch.randint(size=(batch_size,), low=0, high=128, device="cuda", dtype=torch.int)
             batch = next(data_iterator)
             input_ids, labels = batch
             input_ids = input_ids.cuda()
@@ -179,13 +228,13 @@ def main():
             loss = loss_fn(pred, labels)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(
-                sharded_model.parameters(), max_grad_norm, foreach=True
+                sharded_model.parameters(), cfg.max_grad_norm, foreach=True
             )
             optimizer.step()
             
             time_taken = time.time() - start_time
-            logger.info(f"Iteration {i} complete. Time taken: {time_taken}. MFU: {mfu_flops_per_bs * batch_size / time_taken / 1e12} TFLOPs/s")
-            prof.step()
+            logger.info(f"Iteration {i} complete. Time taken: {time_taken}. MFU: {mfu_flops_per_bs * cfg.train_batch_size / time_taken / 1e12} TFLOPs/s")
+            torch_profiler.step()
 
     if dist.get_rank() == 0:
         logger.info("Sleeping 2 seconds for other ranks to complete")
--- src/maester/model.py
+++ src/maester/model.py
@@ -63,7 +63,7 @@ class ModelArgs:
 
 transformer_configs = {
     "CodeLlama-7b-Python-hf": dict(block_size=16384, vocab_size=32000, n_layer=32, dim = 4096, rope_base=1000000),
-    "test": dict(n_layer=16, n_head=32, dim=4096, n_local_heads=8),
+    "test": dict(n_layer=32, n_head=32, n_local_heads=8, dim=4096, intermediate_size=14336, vocab_size=128256),
     "7B": dict(n_layer=32, n_head=32, dim=4096),
     "13B": dict(n_layer=40, n_head=40, dim=5120),
     "30B": dict(n_layer=60, n_head=52, dim=6656),
