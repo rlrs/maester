2fee0c74836108f2e16004a06bcac65783f1bff0
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2024-07-13 22:46:59 +0300

    add 1B llama3

--- src/maester/models/llama/__init__.py
+++ src/maester/models/llama/__init__.py
@@ -30,8 +30,26 @@ llama2_configs = {
 
 llama3_configs = {
     "debugmodel": ModelArgs(dim=1024, n_layers=4, n_heads=16, rope_theta=500000),
+    "430M": ModelArgs( # "430M" with llama2 vocab size (32000)
+        dim=1024,
+        n_layers=24,
+        n_heads=32,
+        n_kv_heads=8,
+        ffn_dim_multiplier=1.3,
+        multiple_of=1024,
+        rope_theta=500000,
+    ),
     "500M": ModelArgs(
         dim=1024,
+        n_layers=16,
+        n_heads=32,
+        n_kv_heads=8,
+        ffn_dim_multiplier=1.3,
+        multiple_of=1024,
+        rope_theta=500000,
+    ),
+    "1B": ModelArgs(
+        dim=2048,
         n_layers=16,
         n_heads=32,
         n_kv_heads=8,
