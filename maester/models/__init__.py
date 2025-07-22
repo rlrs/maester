34d1dca69696b4498db3a22d69d538b20c097831
peter-sk <petersk@imada.sdu.dk>
2025-07-22 14:57:57 +0200

    model name to parallelize fn

--- maester/models/__init__.py
+++ maester/models/__init__.py
@@ -6,6 +6,7 @@
 
 from maester.models.llama import llama2_configs, llama3_configs, mistral_configs, Transformer
 from maester.models.gemma import gemma3_configs, GemmaTextModel
+from maester.parallelisms import parallelize_gemma, parallelize_llama
 
 models_config = {
     "llama2": llama2_configs,
@@ -26,4 +27,11 @@ model_name_to_tokenizer = {
     "llama3": "tiktoken",
     "mistral": "sentencepiece",
     "gemma3": "sentencepiece",
+}
+
+model_name_to_parallelize = {
+    "llama2": parallelize_llama,
+    "llama3": parallelize_llama,
+    "mistral": parallelize_llama,
+    "gemma3": parallelize_gemma,
 }
