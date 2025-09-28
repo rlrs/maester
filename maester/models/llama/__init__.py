f8021d5f7ff8bd1ed4db6cf4360ef9a213f6353a
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-09-28 10:14:35 +0200

    coma 7b parameters

--- maester/models/llama/__init__.py
+++ maester/models/llama/__init__.py
@@ -16,6 +16,13 @@ llama2_configs = {
     "271M": ModelArgs(dim=1024, n_layers=16, n_heads=8),
     "1B": ModelArgs(dim=2048, n_layers=18, n_heads=16),
     "7B": ModelArgs(dim=4096, n_layers=32, n_heads=32),
+    "Coma7B": ModelArgs(
+        dim=4096,
+        n_layers=32,
+        n_heads=32,
+        rope_theta=100000.0,
+        max_seq_len=4096,
+    ),
     "13B": ModelArgs(dim=5120, n_layers=40, n_heads=40),
     "26B": ModelArgs(dim=5120, n_layers=80, n_heads=40),
     "70B": ModelArgs(
