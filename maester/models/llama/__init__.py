6a604203d0aedab99dc8c185a2ecf0a6f68a6949
peter-sk <petersk@imada.sdu.dk>
2025-10-07 09:26:22 +0200

    Update __init__.py

--- maester/models/llama/__init__.py
+++ maester/models/llama/__init__.py
@@ -16,7 +16,7 @@ llama2_configs = {
     "271M": ModelArgs(dim=1024, n_layers=16, n_heads=8),
     "1B": ModelArgs(dim=2048, n_layers=18, n_heads=16),
     "7B": ModelArgs(dim=4096, n_layers=32, n_heads=32),
-    "Coma7B": ModelArgs(
+    "Comma7B": ModelArgs(
         dim=4096,
         n_layers=32,
         n_heads=32,
