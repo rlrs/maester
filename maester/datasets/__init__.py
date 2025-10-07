3089467a28000dea6b1a915f0cebae7135a23cf2
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-10-07 09:54:47 +0200

    and back again

---
+++ maester/datasets/__init__.py
@@ -0,0 +1,11 @@
+from .experimental_otf import *
+
+__all__ = [
+    "StreamingDocDataset",
+    "ScalableShardDataset",
+    "SamplingDataset",
+    "PreloadBufferDataset",
+    "BufferDataset",
+    "PreprocessDataset",
+    "build_experimental_data_loader",
+]
